5c41193d942864da846eec369ab220551173b679
rahulcctech <rahul.roy@cctech.co.in>
2024-09-11 13:02:00 +0000

    jenkin added

--- script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Compiling and running Hello World application"
-echo '#include <iostream>' > hello_world.cpp
-echo 'int main() { std::cout << "Hello World" << std::endl; return 0; }' >> hello_world.cpp
-g++ hello_world.cpp -o hello_world
-./hello_world > hello_world_output.xml
